6314fe72b5634716332fdfd5810d8c7ee29d3abf
Miroir Raphael <miroir.raphael07@gmail.com>
2025-04-03 18:11:15 +0200

    responsive hero section :: v1 & mixin added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Header from "../components/Header";
+import { NavLink } from "react-router";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <Header />
+      <section className="hero-section">
+        <div className="l-part">
+          <h1>
+            <span>From the field to victory, </span>
+            follow every progress
+          </h1>
+          <div className="action-buttons">
+            <button type="button" className="discover-btn">
+              Discover
+            </button>
+            <NavLink to="/register" className="register-btn">
+              <button type="button">Get started for free</button>
+            </NavLink>
+          </div>
+        </div>
+        <div className="r-part">
+          <div className="img"></div>
+        </div>
+      </section>
     </div>
   );
 };
