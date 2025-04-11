2e92a9b55fd637423868fb286b7431b0ea51be4c
Miroir Raphael <miroir.raphael07@gmail.com>
2025-04-11 21:59:55 +0200

    sidebar added :: v1

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
+import { LayoutDashboard } from "lucide-react";
 import React from "react";
+import { NavLink } from "react-router";
 
 const Sidebar = () => {
   return (
@@ -8,7 +10,29 @@ const Sidebar = () => {
           <img src="./img/logo.png" alt="logo TeamTracker" />
         </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <ul className="links">
+          <li>
+            <LayoutDashboard />
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Équipes</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Messages</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Statistiques</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard">Paramètres</NavLink>
+          </li>
+        </ul>
+      </div>
     </aside>
   );
 };
