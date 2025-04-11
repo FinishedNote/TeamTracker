005b39606548dd5ef7261171418eb8b36c1b72f3
Nathan <chippy8708@gmail.com>
2025-04-11 10:23:03 +0200

    translation retired and passed to french

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./scss/main.scss";
-import "./i18n.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,40 +4,38 @@ import { NavLink } from "react-router";
 import { useTranslation } from "react-i18next";
 
 const Home = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="home">
-      <Header />
-      <section className="hero-section">
-        <div className="l-part">
-          <h1>
-            <span>{t("welcome1")}</span>
-            {t("welcome2")}
-          </h1>
-          <div className="action-buttons">
-            <button type="button" className="discover-btn">
-              Discover
-            </button>
-            <NavLink to="/register" className="register-btn">
-              <button type="button">Get started for free</button>
-            </NavLink>
-          </div>
+    return (
+        <div className="home">
+            <Header />
+            <section className="hero-section">
+                <div className="l-part">
+                    <h1>
+                        <span>{t("welcome1")}</span>
+                        {t("welcome2")}
+                    </h1>
+                    <div className="action-buttons">
+                        <button type="button" className="discover-btn">
+                            Discover
+                        </button>
+                        <NavLink to="/register" className="register-btn">
+                            <button type="button">Get started for free</button>
+                        </NavLink>
+                    </div>
+                </div>
+                <div className="r-part">
+                    <div className="img">
+                        <img src="./img/img.png" alt="image" />
+                    </div>
+                </div>
+            </section>
+            <section className="qualities-section">
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+            </section>
+            <section className="prices-section"></section>
         </div>
-        <div className="r-part">
-          <div className="img">
-            <img src="./img/img.png" alt="image" />
-          </div>
-        </div>
-      </section>
-      <section className="qualities-section">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-      </section>
-      <section className="prices-section"></section>
-    </div>
-  );
+    );
 };
 
 export default Home;
