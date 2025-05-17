c36df57c678795655405ab317af5072a8a292763
Nathan <chippy8708@gmail.com>
2025-05-17 14:02:53 +0200

    Refactor AddMember and TeamCard components for improved functionality and styling; update members API to handle added_by field and enhance error handling.

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,12 +3,12 @@ import img from "../assets/img.png";
 import { NavLink } from "react-router-dom";
 
 const TeamCard = ({ team }) => {
-  return (
-    <NavLink to={`/dashboard/teams/${team.name}`} className="team-card">
-      <img src={img} alt="img" />
-      <p>{team.name}</p>
-    </NavLink>
-  );
+    return (
+        <NavLink to={`/dashboard/teams/${team.id}`} className="team-card">
+            <img src={img} alt="img" />
+            <p>{team.name}</p>
+        </NavLink>
+    );
 };
 
 export default React.memo(TeamCard);
--- src/redux/features/members/membersAPI.js
+++ src/redux/features/members/membersAPI.js
@@ -3,41 +3,51 @@ import supabase from "../../../supabaseClient";
 const table = "team_members";
 
 export async function fetchMembers() {
-  const { data, error } = await supabase.from(table).select("*");
-  if (error) throw error;
-  return data;
+    const { data, error } = await supabase.from(table).select("*");
+    if (error) throw error;
+    return data;
 }
 
 export async function createMembers(
-  { user_id, team_id, role, userId },
-  { rejectWithValue }
+    { user_id, team_id, role, userId },
+    { rejectWithValue }
 ) {
-  const { data, error } = await supabase
-    .from(table)
-    .insert([{ user_id, team_id, role, added_by: userId }])
-    .select()
-    .single();
+    console.log("test1");
+    console.log("user id ", user_id);
+    console.log("team id ", team_id);
+    console.log("role ", role);
+    console.log("UserId ", userId);
 
-  if (error) {
-    return rejectWithValue(error.message);
-  }
-  return data;
+    const { data, error } = await supabase
+        .from(table)
+        .insert([{ user_id, team_id, role, added_by: userId }])
+        .select()
+        .single();
+
+    if (error) {
+        console.log(error);
+        return rejectWithValue(error.message);
+    }
+    return data;
 }
 
 export async function updateMembers(user_id, updatedValues) {
-  const { data, error } = await supabase
-    .from(table)
-    .update(updatedValues)
-    .eq("user_id", user_id)
-    .select()
-    .single();
+    const { data, error } = await supabase
+        .from(table)
+        .update(updatedValues)
+        .eq("user_id", user_id)
+        .select()
+        .single();
 
-  if (error) throw error;
-  return data;
+    if (error) throw error;
+    return data;
 }
 
 export async function deleteMembers(user_id) {
-  const { error } = await supabase.from(table).delete().eq("user_id", user_id);
-  if (error) throw error;
-  return { id };
+    const { error } = await supabase
+        .from(table)
+        .delete()
+        .eq("user_id", user_id);
+    if (error) throw error;
+    return { id };
 }
